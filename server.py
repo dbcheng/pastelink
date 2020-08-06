fc0d56617b20181d15592f7de6e58d01ae57a7d4
David Cheng <dcheng@davids-mbp-4.myfiosgateway.com>
2020-08-05 23:47:21 -0400

    Add Login basics to Server

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
 import sqlite3
 from os import path
+import flask
 from flask import Flask, request, g
+from flask_login import LoginManager, login_required
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.secret_key = '9d9da5d5d4dfa53f5e10a600'
 
 DATABASE = 'database.db'
 SCHEMA = 'schema.sql'
 RETRIEVE_URL = 'http://127.0.0.1:5000/retrieve/{}'
+
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    password = PasswordField('Password')
+    submit = SubmitField('Submit')
 
 def init_db():
     # TODO: Change this to checking if table exists
@@ -41,6 +55,42 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(somewhere)
+
+@app.route("/test", methods=['GET'])
+@login_required
+def test_login():
+    return "Login works"
 
 @app.route('/retrieve/<int:paste_id>', methods=['GET'])
 def retrieve(paste_id):
